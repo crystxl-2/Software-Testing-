95354252b1c680f7705fe53f7c52315c7f48d8e0
crystxl-2 <carl.deguzman612@gmail.com>
2026-06-05 00:14:29 +0930

    s

--- test/java/edu/flinders/timetable/ClassServiceTest.java
+++ test/java/edu/flinders/timetable/ClassServiceTest.java
@@ -1,5 +1,13 @@
 package edu.flinders.timetable;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+class ClassServiceTest {
+    private ClassService classService;
+
+
+
+    @Test
+    void testDeleteRecord
--- test/java/edu/flinders/timetable/TimeTableService.java
+++ test/java/edu/flinders/timetable/TimeTableService.java
@@ -3,5 +3,3 @@ package edu.flinders.timetable;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-
-// this is a test mf
